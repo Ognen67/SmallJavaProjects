bceb4e02fc3c1f992a77f4ea27c152723a7fd5f0
Ognen Kirov Todorovski <todorovskiognen@gmail.com>
2021-01-21 23:08:39 +0100

    TicTacToe Version 2.0

--- TicTacToe/src/main/java/TicTacToe.java
+++ TicTacToe/src/main/java/TicTacToe.java
@@ -20,52 +20,33 @@ public class TicTacToe {
     }
 
     private static boolean checkWin(char[][] board, char last) {
+        if (checkRows(board, last)) return true;
+        if (checkColumns(board, last)) return true;
+        if (checkPrimaryDiagonal(board, last)) return true;
+        return checkSecondaryDiagonal(board, last);
+    }
+
+    private static boolean checkSecondaryDiagonal(char[][] board, char last) {
         int count = 0;
-        if (checkRows(board, last, count)) return true;
-        count = 0;
-        if (checkColumns(board, last, count)) return true;
-        count = 0;
-        if (checkPrimaryDiagonal(board, last, count)) return true;
-        count = 0;
-        if (checkSecondaryDiagonal(board, last, count)) return true;
+        if (board[0][2] == last) count++;
+        if (board[1][1] == last) count++;
+        if (board[2][0] == last) count++;
 
-        return false;
+        return count == 3;
     }
 
-    private static boolean checkSecondaryDiagonal(char[][] board, char last, int count) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (i + j == 4) {
-                    if (board[i][0] == last) {
-                        count++;
-                        if (count == 3) {
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
-        return false;
+    private static boolean checkPrimaryDiagonal(char[][] board, char last) {
+        int count = 0;
+        if (board[0][0] == last) count++;
+        if (board[1][1] == last) count++;
+        if (board[2][2] == last) count++;
+
+        return count == 3;
     }
 
-    private static boolean checkPrimaryDiagonal(char[][] board, char last, int count) {
+    private static boolean checkColumns(char[][] board, char last) {
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (i == j) {
-                    if (board[i][0] == last) {
-                        count++;
-                        if (count == 3) {
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    private static boolean checkColumns(char[][] board, char last, int count) {
-        for (int i = 0; i < 3; i++) {
+            int count = 0;
             for (int j = 0; j < 3; j++) {
                 if (board[j][i] == last) {
                     count++;
@@ -78,8 +59,9 @@ public class TicTacToe {
         return false;
     }
 
-    private static boolean checkRows(char[][] board, char last, int count) {
+    private static boolean checkRows(char[][] board, char last) {
         for (int i = 0; i < 3; i++) {
+            int count = 0;
             for (int j = 0; j < 3; j++) {
                 if (board[i][j] == last) {
                     count++;
@@ -97,8 +79,6 @@ public class TicTacToe {
         char board[][] = new char[3][3];
         Scanner input = new Scanner(System.in);
         boolean isX = true;
-        int value;
-        int count = 1;
 
         initializeBoard(board);
 
@@ -106,33 +86,70 @@ public class TicTacToe {
 
         while (true) {
             // Input from 1-9
-            value = input.nextInt();
-
-            if (isValid(value)) {
-                count = 1;
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        if (value == count) {
-                            board[i][j] = isX ? 'X' : 'O';
-                            isX = !isX;
-                            if (checkWin(board, board[i][j])) {
-                                System.out.println("The Winner is: " + board[i][j]);
-                            }
-                        }
-                        count++;
-
-                    }
-                }
-            } else {
-                System.out.println("Invalid input");
-            }
-
+            int value = getValue(input, board);
+            boolean isNextPlayerX = placeTile(board, isX, value);
             printBoard(board);
+
+            if (checkWin(board, isX ? 'X' : 'O')) {
+                System.out.println("The Winner is: " + (isX ? 'X' : 'O'));
+                break;
+            }
+            if (checkTie(board)) {
+                System.out.println("It's a tie");
+                break;
+            }
+            isX = isNextPlayerX;
         }
     }
 
-    private static boolean isValid(int value) {
-        return value > 0 && value < 10;
+    private static boolean placeTile(char[][] board, boolean isX, int value) {
+        int count = 1;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (value == count) {
+                    board[i][j] = isX ? 'X' : 'O';
+                    isX = !isX;
+
+                }
+                count++;
+            }
+        }
+        return isX;
+    }
+
+    private static boolean checkTie(char[][] board) {
+        for (char[] chars : board) {
+            for (char aChar : chars) {
+                if (aChar == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static int getValue(Scanner input, char[][] board) {
+        while (true) {
+            try {
+                String valueLine = input.nextLine();
+                int value = Integer.parseInt(valueLine);
+                if (isValid(value, board)) {
+                    return value;
+                }
+            } catch (NumberFormatException e) {
+                // Do nothing
+            }
+            System.out.println("Invalid input");
+        }
+    }
+
+    private static boolean isValid(int value, char[][] board) {
+        if (value <= 0 || value >= 10) {
+            return false;
+        }
+
+        return board[(value - 1) / 3][(value - 1) % 3] == '-';
     }
 
 
