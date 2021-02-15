915db6c47c0b1e964ec223db258619804abef8d5
Ognen Kirov Todorovski <35491291+Ognen67@users.noreply.github.com>
2021-02-16 00:52:24 +0100

    Chess Initializing Board

---
+++ Chess/src/main/java/Knight.java
@@ -0,0 +1,20 @@
+public class Knight extends Piece {
+
+    public Knight(String pieceColor, int[] piecePosition) {
+        super(pieceColor, piecePosition);
+        this.pieceValue = 3;
+    }
+
+    public int[] movePiece() {
+
+
+        return new int[0];
+    }
+
+    public int[][] eatEnemyPiece() {
+
+
+        return new int[0][];
+    }
+
+}
---
+++ Chess/src/main/java/Queen.java
@@ -0,0 +1,20 @@
+public class Queen extends Piece {
+
+    public Queen(String pieceColor, int[] piecePosition) {
+        super(pieceColor, piecePosition);
+        this.pieceValue = 9;
+    }
+
+    public int[] movePiece() {
+
+
+        return new int[0];
+    }
+
+    public int[][] eatEnemyPiece() {
+
+
+        return new int[0][];
+    }
+
+}
